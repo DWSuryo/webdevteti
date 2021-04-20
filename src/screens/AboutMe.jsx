e256d48909b8cdd5c81ece82579c559dfe92191a
DWLight <72719372+DWSuryo@users.noreply.github.com>
2021-04-20 17:21:43 +0700

    Update AboutMe.jsx

--- src/screens/AboutMe.jsx
+++ src/screens/AboutMe.jsx
@@ -2,7 +2,8 @@ import Card from "../Card";
 
 export default function AboutMe() {
     return (
-        <Card title="Hello, world!">
+        <Card title="Ehe">
+            What do you mean "ehe?"<br><br>
             TETI 2017. Yes, I'm old enough
         </Card>
     );

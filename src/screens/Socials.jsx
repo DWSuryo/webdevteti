762a7187e68d37e3ce99a1b1baf8a485f664611c
DWLight <72719372+DWSuryo@users.noreply.github.com>
2021-04-20 16:54:18 +0700

    Update Socials.jsx

--- src/screens/Socials.jsx
+++ src/screens/Socials.jsx
@@ -4,8 +4,8 @@ export default function Socials() {
     return (
         <Card title="Connect with me">
             <ul>
-                <li>Twitter: <a href="https://twitter.com/comfyte">@username</a></li>
-                <li>Discord: username#0000</li>
+                <!--<li>Twitter: <a href="https://twitter.com/comfyte">@username</a></li>-->
+                <li>reddit: /u/Famas_1234</li>
             </ul>
         </Card>
     );
